6336c380e4c6991f7da08a77d9b862854ff27fe2
AvariceBam <ardi.dehestani@gmail.com>
2025-04-27 23:55:40 -0700

    Hour 10
    Chaotical Spheroids

---
+++ AAmirsoleymany_Hour10/Assets/Improving the Game.cs
@@ -0,0 +1,20 @@
+/*
+ * The Game is neither too easy nor too hard. Due to the element of randomness from the chaos balls, the colored balls can refuse to cooperate and get in their goals and make it challenging, or get knocked into their own goals immediately
+ * 
+ * 
+ * To make the game harder, one could add more balls, more chaos, increase their speeds, make the player slower etc. To make the game easier, one could remove the chaos balls, and make the player bumper faster.
+ * 
+ * One thing that could give it a wow factor is explosions on goal ig. There's not alot of things that can give this game a 'wow' factor, which just leaves excessive visual clutter.
+ * 
+ * The game is fun in concept, or rather the word I would use is amusing. The bouncing balls of chaos make it challenging and tough, and one of the balls solving itself while I was trying to get a different one got a smile outta me. One thing that makes it tedious is the chaos balls. It took longest to get the red ball in due to the chaos balls repeatedly knocking it away from where I was directing it.
+ * 
+ * 
+ * Changes implemented;
+ * 
+ * I added some variety to the chaos balls
+ * I changed the size of the players bumper
+ * (and adjusted player speed to compensate)
+ * I made the bumper more complex(ish)
+ * I removed the water (it was overheating my laptop) and added a grass background.
+ * 
+*/ 
