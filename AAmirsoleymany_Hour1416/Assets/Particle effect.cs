3d72d58946be95408a3d0e89879a2ce1e5404903
AvariceBam <ardi.dehestani@gmail.com>
2025-05-17 13:37:03 -0700

    Hour 14 and 16
    Did all the TIY's for each hour in one scene

---
+++ AAmirsoleymany_Hour1416/Assets/Particle effect.cs
@@ -0,0 +1,11 @@
+// Made this cool lightning effect by messing with things, so I might miss a thing or 2.
+// First is a color modification into a blue effect,
+//Then I gave it Trails
+//Then I modified those trails to be Ribbons
+// I gave the effect some gravity, not alot, but make it more squished ,
+// Then I Lowered the Max Particles,
+// Gave the generation some curve
+// Increased the Sim. Speed
+// Gave it some rotation speed
+// Some Light effects, (Which may or may not function)
+// And I *THINK* that was everything I kept on it.
