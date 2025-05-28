c44a3c4bf9606a21cf7866f1165ddbd07c8b2d04
AvariceBam <ardi.dehestani@gmail.com>
2025-05-27 21:47:29 -0700

    Hour 20

---
+++ AAmirsoleymany_Hour20/Assets/GameCritic.cs
@@ -0,0 +1,17 @@
+/*
+ * 
+ * Game is too easy, Power ups too big, obstacles too small. 
+ * Changes I would make, shrink the power ups, grow the obstacles.
+ * To give "Wow" factor, give it explosions and maybe a catchy soundtrack
+ * Reasons why its fun;
+ * The game feels like a discount subway surfer.
+ * Its not difficult, but it takes patience combined with skill to actually get anywhere, the amount of obstacles can make loss of focus in a competetive setting very hard
+ * Reasons why its tedious
+ * The game feels like a discount subway surfer.
+ * The game is slow, and has no real incentive to play. There are not coins, or an active score except what you get at the end. 
+ * 
+ * 
+ * 
+ * Changes, I increased the cube size to be longer, so the phase button has purpose now. Kept it red though, standard stuff.
+ * and I am going to mess with the power up by giving it light blue particles, rather than yellow on yellow.
+ * */
