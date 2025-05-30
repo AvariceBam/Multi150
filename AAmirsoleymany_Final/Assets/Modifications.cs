b22b6fac02e3d5bcadb3dc74066030f3f428469b
AvariceBam <ardi.dehestani@gmail.com>
2025-05-29 23:01:29 -0700

    FINAL GAUNTLET RUNNER
    Changes listed inside.

---
+++ AAmirsoleymany_Final/Assets/Modifications.cs
@@ -0,0 +1,7 @@
+/* 1) I have changed the directional light color and intensity,
+ * 2) I have added pink fog at a low intensity
+ * 3) I have messed with the skybox
+ * 4) I made the walls prefabs, and tried to alter them slightly, .
+ * 5) Made a new obstacle, looking somewhat like a glowing rock with red gems coming out of it with a blue base that doesn't look normal. In truth,the base is a png of a skybox I found online that I decided would be strange enough to fit here. The red parts of the rock also glow red to further enhance the "obstacle"-ness of it. It also emits red particles with red trails. I messed with emission, gave it trails, changed the sizes etc.
+ * 5) I made the powerup/point glow green, as that along with yellow or the two colors that tell me what is "good". Also I moved them a bit down for better clarity of sight
+ * I think that about covers it. I did this last so I could have a chance to refresh over my actions and make sure I did everything, and I feel as if I have covered all the changes I made. */
